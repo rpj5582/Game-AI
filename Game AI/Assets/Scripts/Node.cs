87bb5254a6f58da32aa1ebe9a9d346384ec9283e
John Miller <jamiller40@hotmail.com>
2018-02-25 01:51:42 -0500

    First pass on node grid spawning. Nodes spawn in a grid over the terrain, at the proper heights. Bridges are not taken into account yet

---
+++ Game AI/Assets/Scripts/Node.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+    private Vector3 pos;
+    private float cost;
+
+    private GameObject visualization, visContainer;
+
+    //Data structure for information about a node. The visulization object is just for debuggin
+    public Node(Vector3 _pos, float _cost, GameObject _visualizationPrefab, GameObject _visContainer) {
+        pos = _pos;
+        cost = _cost;
+
+        if(NodeManager.visualizeNodes) {
+            visContainer = _visContainer;
+
+            visualization = GameObject.Instantiate(_visualizationPrefab, pos, Quaternion.identity);
+            visualization.transform.parent = visContainer.transform;
+        }
+    }
+
+    public float CalculateCost() {
+        return Mathf.Infinity;
+    }
+}
---
+++ Game AI/Assets/Scripts/NodeManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager : MonoBehaviour {
+    [SerializeField]
+    private int totalRowNodes;
+
+    private Node[,]nodes;
+
+    //Debugging object
+    [SerializeField]
+    private GameObject visualizationPrefab;
+
+    //Parent all the node visualization objects to this object for neatness
+    [SerializeField]
+    private GameObject nodeVisContainer;
+
+    //If false, no node visualization objects will be created
+    public static bool visualizeNodes = true;
+
+    void Awake () {
+        SpawnNodes();
+    }
+    
+    private void SpawnNodes() {
+        nodes = new Node[totalRowNodes,totalRowNodes];
+
+        Vector3 spawnPos = Vector3.zero;
+        float xPos = 0; 
+        float zPos = 0;
+
+        //Extents works like radius, so needs to be multiplied by 2
+        float terrainWidth = Terrain.activeTerrain.terrainData.bounds.extents.x * 2;
+
+        //Spawn rows then columns
+        for(int i = 0; i < totalRowNodes; i++) {
+            for(int j = 0; j < totalRowNodes; j++) {
+                //Find the x and z coordinate of this node then add it to the terrain's world position to get it's world position
+                //Space inbtween each node scales with terrain width
+                xPos = Terrain.activeTerrain.terrainData.bounds.min.x + (i * (terrainWidth / totalRowNodes));
+                zPos = Terrain.activeTerrain.terrainData.bounds.min.y + (j * (terrainWidth / totalRowNodes));
+
+                spawnPos = new Vector3(xPos, Terrain.activeTerrain.SampleHeight(new Vector3(xPos, 0, zPos)), zPos);
+                Node newNode = new Node(spawnPos, 0.5f, visualizationPrefab, nodeVisContainer);
+                nodes[i, j] = newNode;
+            }
+        }
+    }
+}
